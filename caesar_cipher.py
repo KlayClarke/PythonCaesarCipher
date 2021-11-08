223672a05df3b30aa7eb10a3810e437a90c7ed80
Klay Clarke <klayaclarke@gmail.com>
2021-11-08 09:47:04 -0500

    remove unused import

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -1,5 +1,3 @@
-from collections import deque
-
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',\
             'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',\
             'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
