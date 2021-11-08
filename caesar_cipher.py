371e15e334bc304ec8838bdd68b5ca2904b63006
Klay Clarke <klayaclarke@gmail.com>
2021-11-08 11:27:00 -0500

    add logo, add replay option, refactor code for readability

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -1,24 +1,54 @@
+logo = """           
+ ,adPPYba, ,adPPYYba,  ,adPPYba, ,adPPYba, ,adPPYYba, 8b,dPPYba,  
+a8"     "" ""     `Y8 a8P_____88 I8[    "" ""     `Y8 88P'   "Y8  
+8b         ,adPPPPP88 8PP"""""""  `"Y8ba,  ,adPPPPP88 88          
+"8a,   ,aa 88,    ,88 "8b,   ,aa aa    ]8I 88,    ,88 88          
+ `"Ybbd8"' `"8bbdP"Y8  `"Ybbd8"' `"YbbdP"' `"8bbdP"Y8 88   
+            88             88                                 
+           ""             88                                 
+                          88                                 
+ ,adPPYba, 88 8b,dPPYba,  88,dPPYba,   ,adPPYba, 8b,dPPYba,  
+a8"     "" 88 88P'    "8a 88P'    "8a a8P_____88 88P'   "Y8  
+8b         88 88       d8 88       88 8PP""""""" 88          
+"8a,   ,aa 88 88b,   ,a8" 88       88 "8b,   ,aa 88          
+ `"Ybbd8"' 88 88`YbbdP"'  88       88  `"Ybbd8"' 88          
+              88                                             
+              88           
+"""
+
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',\
-            'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',\
             'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
-
-text_length = len(text)
+print(logo)
 
 def caesar(direction,text, shift):
     new_text=''
-    for letter in text:
-        if direction=='encode':
-            letter_index=alphabet.index(letter)
-            new_letter=alphabet[letter_index+shift]
-            new_text+=new_letter
-        elif direction=='decode':
-            letter_index=alphabet.index(letter)
-            new_letter=alphabet[letter_index-shift]
-            new_text+=new_letter
-    print(f'Here is the {direction} result: {new_text}')
+    for char in text:
+        if char in alphabet:
+            if shift_direction=='encode':
+                char_index=alphabet.index(char)
+                new_char=alphabet[char_index+shift_amount]
+                new_text+=new_char
+            elif shift_direction=='decode':
+                char_index=alphabet.index(char)
+                new_char=alphabet[char_index-shift]
+                new_text+=new_char
+        else:
+            new_text+=char
+    print(f'Here is the {direction} result: {new_text}\n')
 
-caesar(direction,text,shift)    
+should_continue = True
+
+while should_continue:
+    
+    shift_direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    user_text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    shift_amount = shift % 26
+    caesar(direction=shift_direction,text=user_text,shift=shift_amount)
+    result = input('Type yes if you would like to run again. Otherwise type no.\n')
+    if result == 'no':
+        should_continue = False
+        print('Goodbye')   
+
+      
